ea00a6bfdd49364d49d88c80db13a0f3e97feb21
Talha-542 <ta662561@gmail.com>
2024-10-20 21:48:46 +0500

    homepage sections (first commit , repository switched)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import Header from "./components/Header/Header";
+import "./App.css";
+import Hero from "./components/Hero/Hero";
+import Companies from "./components/Companies/Companies";
+import Residencies from "./components/Residencies/REsidencies";
+import Value from "./components/Value/Value";
 
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="app">
+      <Header />
+      <Hero />
+      <Companies />
+      <Residencies />
+      <Value/>
+    </div>
   );
 }
 
